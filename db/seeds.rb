df9b20e4cecfba659a5b410d502161f77062c790
Sergio <sergioh.c@live.com>
2018-07-24 21:53:22 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # Loads seed data out of default dir
-Dir[File.join(__dir__, 'default', '*.rb')].each { |file| require file }
-
+#Dir[File.join(__dir__, 'default', '*.rb')].each { |file| require file }
+default_path = File.join(File.dirname(__FILE__), 'default')
 Rake::Task['db:load_dir'].reenable
 Rake::Task['db:load_dir'].invoke(default_path)
